484e12decec93e1d5927230c48add7b4fb2408f3
Jacob Schuster <jschuster8765@gmail.com>
2023-11-22 01:27:19 -0500

    initial dashboard page

---
+++ src/app/components/FeedItem.tsx
@@ -0,0 +1,157 @@
+import Link from "next/link";
+
+export interface FeedItemProps {
+  type: "legislation" | "news" | "event" | "discussion" | "poll";
+  title: string;
+  description: string;
+  date: Date;
+  id: string;
+}
+
+const FEED_ITEM_TYPE_THEME = {
+  legislation: {
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="h-6 w-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z"
+        />
+      </svg>
+    ),
+    color: "bg-blue-200/20",
+    hoverColor: "hover:bg-blue-200/40",
+  },
+  news: {
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="h-6 w-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z"
+        />
+      </svg>
+    ),
+    color: "bg-green-200/20",
+    hoverColor: "hover:bg-green-200/40",
+  },
+  event: {
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="h-6 w-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
+        />
+      </svg>
+    ),
+    color: "bg-slate-200/20",
+    hoverColor: "hover:bg-slate-200/40",
+  },
+  discussion: {
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="h-6 w-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"
+        />
+      </svg>
+    ),
+    color: "bg-purple-200/20",
+    hoverColor: "hover:bg-purple-200/40",
+  },
+  poll: {
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="h-6 w-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
+        />
+      </svg>
+    ),
+    color: "bg-red-200/20",
+    hoverColor: "hover:bg-red-200/40",
+  },
+};
+
+export default function FeedItem({
+  type,
+  title,
+  description,
+  date,
+  id,
+}: FeedItemProps) {
+  return (
+    <Link
+      href={`/feed/${id}`}
+      className={`flex flex-col gap-1 rounded-xl ${FEED_ITEM_TYPE_THEME[type].color} p-4 ${FEED_ITEM_TYPE_THEME[type].hoverColor}`}
+    >
+      <div className="flex flex-row items-center gap-2">
+        <div className="flex-grow">
+          <div className="bg-te flex flex-row items-center gap-2">
+            <span className={`font-bold} text-xl`}>
+              {FEED_ITEM_TYPE_THEME[type].icon}
+            </span>
+            <span className="font-serif text-2xl font-bold">{title}</span>
+          </div>
+          <p className="text-md">{description}</p>
+          <p className="text-sm text-gray-400">
+            {date.toLocaleString("en-US")}
+          </p>
+        </div>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="h-8 w-8"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M8.25 4.5l7.5 7.5-7.5 7.5"
+          />
+        </svg>
+      </div>
+    </Link>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,55 @@
+import type { FeedItemProps } from "../components/FeedItem";
+import FeedItem from "../components/FeedItem";
+
+const DUMMY_FEED: FeedItemProps[] = [
+  {
+    type: "legislation",
+    title: "New Legislation Proposal",
+    description:
+      "A new legislation proposal has been introduced for public review.",
+    date: new Date("2023-11-22T10:30:00Z"),
+    id: "1",
+  },
+  {
+    type: "news",
+    title: "City Park Renovation Update",
+    description:
+      "Get the latest news on the progress of the city park renovation project.",
+    date: new Date("2023-11-22T12:45:00Z"),
+    id: "2",
+  },
+  {
+    type: "event",
+    title: "Community Cleanup Day",
+    description:
+      "Join us for a community cleanup day on Saturday. Let's work together to keep our neighborhood clean!",
+    date: new Date("2023-11-23T09:00:00Z"),
+    id: "3",
+  },
+];
+
 export default function Dashboard() {
-  return <h1 className="font-serif text-2xl font-bold">Dashboard</h1>;
+  return (
+    <main className="container mx-auto grid grid-cols-6 gap-4 p-4">
+      <div className="col-span-4 rounded-xl bg-white/20 p-4">
+        <h2 className="font-serif text-2xl font-bold">My Feed</h2>
+        <p className="mt-1 text-lg">
+          View the latest news and updates as they happen.
+        </p>
+
+        <div className="mt-4 grid grid-cols-1 gap-2">
+          {DUMMY_FEED.map((item) => (
+            <FeedItem {...item} key={item.id} />
+          ))}
+        </div>
+      </div>
+
+      <div className="col-span-2 rounded-xl bg-white/20 p-4">
+        <h2 className="font-serif text-2xl font-bold">Filters</h2>
+        <p className="mt-1 text-lg">
+          What type of content are you interested in?
+        </p>
+      </div>
+    </main>
+  );
 }
